49bbaf3adebf8d63668d5cfab07129f475f1e787
Paulo Cesar Crepaldi <crepaldipaulocesar@gmail.com>
2025-11-08 10:14:49 -0300

    Update map_visualizer.py

--- map_visualizer.py
+++ map_visualizer.py
@@ -78,7 +78,10 @@ def _add_colorbar_bottomleft(mapa: geemap.Map, vis_params: dict, unit_label: str
         return 
 
     N_STEPS = len(palette) 
-    index = np.linspace(vmin, vmax, N_STEPS).astype(int) 
+    #index = np.linspace(vmin, vmax, N_STEPS).astype(int)
+
+    step = round((vmax - vmin) / N_STEPS)
+    index = np.arange(vmin, vmax + step, step, dtype=int)
 
     colormap = StepColormap(
         colors=palette, 
@@ -312,4 +315,5 @@ def _stitch_images_to_bytes(title_bytes: bytes, map_bytes: bytes,
     except Exception as e:
         st.error(f"Erro ao costurar imagens: {e}")
         return None
+
 
