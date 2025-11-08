2aafa059bd77f3627fd27232063f8cc9d51fd9ed
Paulo Cesar Crepaldi <crepaldipaulocesar@gmail.com>
2025-11-08 06:51:20 -0300

    Update ui.py

--- ui.py
+++ ui.py
@@ -166,7 +166,6 @@ def renderizar_sidebar(dados_geo, mapa_nomes_uf):
                     **Como usar:**
                     1.  O mapa de desenho aparecerá na tela principal.
                     2.  Use as ferramentas de desenho (⬟ ou ■) no canto esquerdo do mapa.
-                
                     """)
             
             st.divider()
@@ -335,4 +334,5 @@ def renderizar_pagina_sobre():
     
     st.markdown("<hr class='divisor'>", unsafe_allow_html=True)
     st.markdown("<p style='text-align:center;color:gray;font-size:12px;'>Desenvolvido por Paulo C. Crepaldi – CAT314 / UNIFEI</p>", unsafe_allow_html=True)
+
 
