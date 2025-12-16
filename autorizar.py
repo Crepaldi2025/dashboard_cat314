d2f1d02b06d31d7199006e863d758212a9af47c9
Paulo Cesar Crepaldi <crepaldipaulocesar@gmail.com>
2025-12-16 06:28:04 -0300

    Delete autorizar.py

--- autorizar.py
+++
@@ -1,9 +0,0 @@
-import ee
-
-# Inicializa explicitamente o projeto Earth Engine
-ee.Initialize(project='gee-crepaldi-2025')
-
-# Teste simples para confirmar a conexão
-info = ee.Image('NASA/NASADEM_HGT/001').getInfo()
-print("✅ Conexão estabelecida com sucesso!")
-print("Imagem de teste:", info['id'])
